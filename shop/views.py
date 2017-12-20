96726d6b50538f3271de5989110d6310d088b420
smk950226 <smk950226@naver.com>
2017-12-20 23:49:08 +0900

    검색구현

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,17 @@ from .models import Item
 
 class ItemListView(ListView):
     model = Item
+
+    def get_queryset(self):
+        self.q = self.request.GET.get('q', '')
+
+        qs = super().get_queryset()
+        
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.qs
+        return context
