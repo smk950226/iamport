0a30dca50a9e310d8ec7e158432f22a1b8ce67f6
smk950226 <smk950226@naver.com>
2017-12-20 23:11:31 +0900

    login, profile 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
 
 urlpatterns = [
-    
+    url(r'^login/$', login, name='login', kwargs={'template_name': 'accounts/login.html'}),
+    url(r'^profile/$', views.profile, name="rofile"),
 ]
