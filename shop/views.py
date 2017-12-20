0b2b72e50fc020a498054e0f0d8a670d2ab81a65
smk950226 <smk950226@naver.com>
2017-12-20 23:49:58 +0900

    검색구현 수정

--- shop/views.py
+++ shop/views.py
@@ -16,5 +16,5 @@ class ItemListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['q'] = self.qs
+        context['q'] = self.q
         return context
