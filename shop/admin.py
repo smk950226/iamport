f32bd9317e5355697861109bfc0328b40ce7c767
smk950226 <smk950226@naver.com>
2017-12-20 23:34:23 +0900

    admin에 상품사진 노출

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Item
+from django.utils.safestring import mark_safe
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'amount']
+    list_display = ['img', 'name', 'amount']
+    list_display_links = ['img', 'name']
+
+    def img(self, item):
+        if item.photo:
+            return mark_safe('<img src="{}" style="width: 75px;">'.format(item.photo.url))
+        return None
