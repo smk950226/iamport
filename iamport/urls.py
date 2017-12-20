4eca1413cbbd5254b9380e0b6da77d0c3b23d0f8
smk950226 <smk950226@naver.com>
2017-12-20 23:21:55 +0900

    media 설정

--- iamport/urls.py
+++ iamport/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,3 +25,5 @@ urlpatterns = [
     url(r'^$', lambda r:redirect('shop:index'), name='root'),
     url(r'^accounts/', include('accounts.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
